b8864f76d7ee462dc28fdd2dc740bb9f0b0bb532
AllaHoffman <sm1th303@gmail.com>
2014-07-04 20:19:38 -0400

    Changed new character form over to rails form helpers

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -7,17 +7,24 @@ class CharactersController < ApplicationController
   end
 
   def show
-    
+    @character = Character.find(params[:id])
   end
 
   def new
+    @character = Character.new
   end
 
   def create
+    @character = current_user.character.build(character_params)
   end
 
   private
 
   def character_params
+    #will want to change metatype form to dropdown
+    #take cars systems check as model
+    params.require(:character).permit(:name, :metatype_id, :body, :agilty,
+      :reaction, :strength, :charisma, :intuition, :logic, :willpower, :edge,
+      :essence, :magic, :initiative_passes, :nuyen, :bio)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'character#index'
+  root 'characters#index'
 
   resources :characters, only: [:index, :show, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
