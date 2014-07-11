88cca7df6e699876350b56740ca9e5de2287c50f
AllaHoffman <sm1th303@gmail.com>
2014-07-11 10:59:38 -0400

    alerts you to glitches and critical glitches

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -41,10 +41,36 @@ $( document ).ready(function() {
         results.push(Math.floor(Math.random() * (6 - 1 + 1)) + 1);
       }
 
+      var successes = [];
+      var glitchThreat = [];
+
+      results.sort
+      for(i=0; i < results.length; i++) {
+        if(results[i] >= 4) {
+          successes.push(results[i]);
+        } else if (results[i]===1) {
+          glitchThreat.push(results[i]);
+        }
+      }
+
+      var glitch = "";
+
+      if(glitchThreat.length >= (dicepoolValue/2)) {
+        if(successes.length === 0) {
+          glitch = " Critical glitch!";
+        } else {
+          glitch = " Glitch!";
+        }
+      }
+
       var returned = results.join();
+
       var result = $("<li>");
-      result.html("Results: " + returned).css("font-weight", "bold");
-      $('#quick-actions').append(result)
+      result.html("Results: " + returned + " Successes: " +
+        successes.length + glitch).css("font-weight", "bold");
+      $('#quick-actions').append(result);
+
+
     } else {
       alert("Please choose a positive number of dice!");
     }
