27369ab0c2a8d5300d07d10c477ec0fa32484d8c
AllaHoffman <sm1th303@gmail.com>
2014-07-14 10:18:14 -0400

    Closing brackets, working on adding other AJAX besides skills

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -95,21 +95,22 @@ stateChangeForm = function() {
 var ajaxer;
 
 ajaxer = function() {
-  $('#new_character_skill').on('submit', function(event) {
+  $('#new_character_skill' || '#change_character').on('submit', function(event) {
     event.preventDefault();
-
+    alert('hit');
     var dataSubmit = $(this).serialize();
 
     $.ajax({
       type: "POST",
       url: $(this).attr('action'),
       dataType: "json",
-      data: dataSubmit
+      data: dataSubmit,
       success: function() {
 
       }
     });
-  }
+  });
+}
 
 $( document ).ready(gear);
 $(document).on('page:load', gear);
