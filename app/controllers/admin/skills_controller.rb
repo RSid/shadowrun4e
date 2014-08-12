6498365f42e10d941955fd206e4474681d42c1bc
RSid <sm1th303@gmail.com>
2014-08-11 20:59:42 -0400

    displays number of characters using a skill on skills index page

--- app/controllers/admin/skills_controller.rb
+++ app/controllers/admin/skills_controller.rb
@@ -1,5 +1,9 @@
 class Admin::SkillsController < ApplicationController
   def index
     @admin_skills = Skill.all
+    @use_count = []
+    @admin_skills.each do |skill|
+      @use_count << CharacterSkill.all.count(skill: skill)
+    end
   end
 end
