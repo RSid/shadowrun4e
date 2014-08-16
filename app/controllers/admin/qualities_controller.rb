7818eccb7840f7d1b94567e6ac5b7f6949b8058e
RSid <sm1th303@gmail.com>
2014-08-16 11:15:18 -0400

    displays qualites ordered by use frequency on admin dash

--- app/controllers/admin/qualities_controller.rb
+++ app/controllers/admin/qualities_controller.rb
@@ -1,5 +1,6 @@
 class Admin::QualitiesController < ApplicationController
   def index
-    @admin_qualities = Quality.all
+    sql = "SELECT qualities.name, qualities.description, qualities.affected_stat, COUNT(character_qualities.quality_id) FROM qualities LEFT JOIN character_qualities ON qualities.id = character_qualities.quality_id GROUP BY qualities.name,qualities.description, qualities.affected_stat"
+    @admin_qualities = ActiveRecord::Base.connection.execute(sql).to_a.sort_by{|quality| quality["count"]}.reverse!
   end
 end
